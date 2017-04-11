457e86ab15a5b82171d3aad9cd7a2d070dc91be2
Jonas Andersson <jonananas@gmail.com>
2017-04-11 14:15:43 +0200

    Using dynamic port in end-to-end-test

--- src/test/java/se/pvt/ehandel/produkt/infrastructure/ProduktResourceEndToEndTest.java
+++ src/test/java/se/pvt/ehandel/produkt/infrastructure/ProduktResourceEndToEndTest.java
@@ -2,6 +2,7 @@ package se.pvt.ehandel.produkt.infrastructure;
 
 import static com.jayway.restassured.RestAssured.given;
 
+import org.eclipse.jetty.server.NetworkConnector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -13,11 +14,12 @@ import se.pvt.ehandel.produkt.infrastructure.resources.ProduktResource;
 
 public class ProduktResourceEndToEndTest {
 	static ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+	private static int port = -1;
 	
 	@BeforeClass
 	public static void startServer() throws Exception {
 		context.setContextPath("/");
-		Server jettyServer = new Server(8080);
+		Server jettyServer = new Server(0);
 		jettyServer.setHandler(context);
 		
 		ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
@@ -26,13 +28,14 @@ public class ProduktResourceEndToEndTest {
 				ProduktResource.class.getCanonicalName());
 		
 		jettyServer.start();
+		port = ((NetworkConnector)jettyServer.getConnectors()[0]).getLocalPort();
 	}
 	
 	@Test
 	public void shouldNotGetProdukt() throws Exception {
 
 		given().
-			port(8080).
+			port(port).
 		when().
 			get("/produkter/123").
 		then().
@@ -43,6 +46,7 @@ public class ProduktResourceEndToEndTest {
 	public void shouldAddProdukt() throws Exception {
 		
 		given().
+			port(port).
 			body("{\"produktId\":\"123\"}").
 			header("Content-Type", "application/json").
 		when().
@@ -57,6 +61,7 @@ public class ProduktResourceEndToEndTest {
 		shouldAddProdukt();
 		
 		given().
+			port(port).
 		when().
 			get("/produkter/123").
 		then().
@@ -69,6 +74,7 @@ public class ProduktResourceEndToEndTest {
 	public void shouldGetProdukter() throws Exception {
 		
 		given().
+			port(port).
 		when().
 			get("/produkter").
 		then().
